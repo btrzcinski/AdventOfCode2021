82db3107391bbdc46b2e5c88c7c27e70a15660b4
Barnett Trzcinski <btrzcinski@gmail.com>
2021-12-06 22:06:24 -0800

    Day 7: refactor maps to foldr

--- Day7/src/Lib.hs
+++ Day7/src/Lib.hs
@@ -14,8 +14,8 @@ arithmeticSum u = (u * (1 + u)) `div` 2
 minFuelForAlignment :: (Int -> Int) -> [Int] -> Int
 minFuelForAlignment costFn s =
     let possiblePositions = [(minimum s)..(maximum s)]
-        movementCosts = map (\p -> map (costFn . abs . (p -)) s) possiblePositions
-    in minimum (map sum movementCosts)
+    in foldr (\p c -> min c $ foldr (\x c' -> (c' +) $ costFn $ abs (p - x)) 0 s) maxBound possiblePositions
+
 
 minStraightLineFuelForAlignment :: [Int] -> Int
 minStraightLineFuelForAlignment = minFuelForAlignment straightLine
